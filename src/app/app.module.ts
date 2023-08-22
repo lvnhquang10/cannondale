67f3138eb70e0c409494e7be9aa4b1e8aaca0cb6
lvnhquang10 <lv.nhquang10@gmail.com>
2023-08-22 20:47:17 +0700

    header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './component/home/home.component';
+import { BikesComponent } from './component/bikes/bikes.component';
+import { ElectricBikesComponent } from './component/electric-bikes/electric-bikes.component';
+import { GearComponent } from './component/gear/gear.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    BikesComponent,
+    ElectricBikesComponent,
+    GearComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/electric-bikes/electric-bikes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-electric-bikes',
+  templateUrl: './electric-bikes.component.html',
+  styleUrls: ['./electric-bikes.component.css']
+})
+export class ElectricBikesComponent {
+
+}
